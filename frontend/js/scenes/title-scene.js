312da2df3781cf6f7f2f15601878d8b597ac9061
Linkshimcat <axzenone@gmail.com>
2026-09-01 23:37:10 +0900

    feat: redesign title and story dialog scenes

--- frontend/js/scenes/title-scene.js
+++ frontend/js/scenes/title-scene.js
@@ -7,10 +7,13 @@ export function createTitleScene({ root, goTo }) {
     node.className = "scene title-scene";
     node.setAttribute("aria-label", "타이틀 화면");
     node.innerHTML = `
-      <div class="title-scene__heading">
-        <p class="title-scene__eyebrow">오후 6시: 흑역사 전국 생중계 LIVE</p>
-        <h1 class="title-scene__logo">6PM</h1>
-      </div>
+      <h1 class="title-scene__title">
+        <img
+          class="title-scene__logo"
+          src="./assets/images/IntroTitleLogo.png"
+          alt="오후 6시: 흑역사 전국 생중계 파괴 작전"
+        >
+      </h1>
       <ul class="title-scene__menu">
         <li><button type="button" class="title-scene__menu-button" data-action="start">START</button></li>
         <li><button type="button" class="title-scene__menu-button" data-action="option">OPTION</button></li>
